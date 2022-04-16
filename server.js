82c620a6d8d8b9074fc6d406462dc869d098fd12
Ojo Paul <tosinkoa1@gmail.com>
2022-04-16 12:22:31 +0100

    Access control set

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://helpful-puppy-d01b53.netlify.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
--- src/api/Authentication/Login.js
+++ src/api/Authentication/Login.js
@@ -32,7 +32,8 @@ router.post(
 
       res.cookie("token", token, {
         httpOnly: true,
-        sameSite: "strict",
+        secure: true,
+        sameSite: "none",
       }).send;
       res.status(200).json("Success");
     } catch (err) {
